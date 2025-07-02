645e2894ebd22af24622472a4d824ec155900fad
Leandro Cañellas <federico.leandro.c@gmail.com>
2025-07-02 03:07:04 -0400

    Parcha migración y modelo Session.

--- src/migrations/20250630221059-create-user.js
+++ src/migrations/20250630221059-create-user.js
@@ -1,38 +1,38 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('user', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        allowNull: false,
-        type: Sequelize.STRING(40)
-      },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING(60)
-      },
-      isAdmin: {
-        allowNull: false,
-        defaultValue: false,
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('user');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('user', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING(40)
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.STRING(60)
+            },
+            isAdmin: {
+                allowNull: false,
+                defaultValue: false,
+                type: Sequelize.BOOLEAN
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('user');
+    }
 };
--- src/migrations/20250702052318-create-session.js
+++ src/migrations/20250702052318-create-session.js
@@ -1,48 +1,52 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('session', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        allowNull: false,
-        references: {
-          model: 'user',
-          key: 'id'
-        },
-        onUpdat: 'RESTRICT',
-        onDelete: 'RESTRICT',
-        type: Sequelize.INTEGER
-      },
-      sessionId: {
-        allowNull: false,
-        type: Sequelize.STRING(32)
-      },
-      token: {
-        allowNull: false,
-        type: Sequelize.TEXT
-      },
-      active: {
-        allowNull: false,
-        defaultValue: true,
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('session');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('session', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            userId: {
+                allowNull: false,
+                references: {
+                    model: 'user',
+                    key: 'id'
+                },
+                onUpdat: 'RESTRICT',
+                onDelete: 'RESTRICT',
+                type: Sequelize.INTEGER
+            },
+            sessionId: {
+                allowNull: false,
+                type: Sequelize.STRING(36)
+            },
+            token: {
+                allowNull: false,
+                type: Sequelize.TEXT
+            },
+            active: {
+                allowNull: false,
+                defaultValue: true,
+                type: Sequelize.BOOLEAN
+            },
+            expireAt: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('session');
+    }
 };
